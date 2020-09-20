d7f39d9cea45c5848ce8615b84c267cabba2234e
SaloniSS <sxs170015@utdallas.edu>
2020-09-20 02:53:54 -0500

    log unneeded stuff

--- backend/routes/subjectivity-routes.js
+++ backend/routes/subjectivity-routes.js
@@ -4,11 +4,11 @@ const axios = require("axios");
 
 router.get("/", async (req, res, next) => {
   const textToAnalyze = req.query.text;
-  console.log(req.query.text);
+  //console.log(req.query.text);
   const URI = `https://us-central1-saloni-shivdasani.cloudfunctions.net/subjectivity-analyzer?text=${textToAnalyze}`;
   const encodedURI = encodeURI(URI);
   const response = await axios.get(encodedURI);
-  console.log(response.data);
+  //console.log(response.data);
   return res.status(200).json(response.data);
 });
 
