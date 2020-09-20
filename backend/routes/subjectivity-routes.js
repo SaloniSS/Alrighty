402f5d159a244ef90aec713d28200f36a49b4e6f
SaloniSS <sxs170015@utdallas.edu>
2020-09-20 02:53:34 -0500

    encode uri to avoid errors

--- backend/routes/subjectivity-routes.js
+++ backend/routes/subjectivity-routes.js
@@ -4,11 +4,11 @@ const axios = require("axios");
 
 router.get("/", async (req, res, next) => {
   const textToAnalyze = req.query.text;
-  //console.log(req.query.text);
-  const response = await axios.get(
-    `https://us-central1-saloni-shivdasani.cloudfunctions.net/subjectivity-analyzer?text=${textToAnalyze}`
-  );
-  //console.log(response);
+  console.log(req.query.text);
+  const URI = `https://us-central1-saloni-shivdasani.cloudfunctions.net/subjectivity-analyzer?text=${textToAnalyze}`;
+  const encodedURI = encodeURI(URI);
+  const response = await axios.get(encodedURI);
+  console.log(response.data);
   return res.status(200).json(response.data);
 });
 
